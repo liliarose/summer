e7028854f1c59dee74e2f84c5668ff0029bf0e9b
liliarose <wendyzhang250@gmail.com>
2016-06-30 13:10:21 -0700

    Lab 2.3 not exectuable yet, add the initalizations

--- teamG_lab23.cc
+++ teamG_lab23.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std; 
 
@@ -6,10 +7,13 @@ using namespace std;
 	string firstName, lastName, address, city, state, zip, phone; 
 }; 
 or class 
+now can create multiple ContactInfo 
 */
 
 int main(){
 	
-	
+	string firstName, lastName, address, city, state, zip, phone; 
+	//cin or initialize variables 
+	cout << firstName << " " << lastName << endl << address << endl << city << ", " << state << " " << zip << endl << phone << endl; 
 	return 0; 
 }
