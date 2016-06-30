b105c81c07efc15a85549c050fd70cf802b788c2
liliarose <wendyzhang250@gmail.com>
2016-06-30 12:43:46 -0700

    lab 2.3 待定

--- teamG_lab23.cc
+++ teamG_lab23.cc
@@ -1,1 +1,15 @@
 #include <iostream>
+
+using namespace std; 
+
+/*struct ContactInfo{
+	string firstName, lastName, address, city, state, zip, phone; 
+}; 
+or class 
+*/
+
+int main(){
+	
+	
+	return 0; 
+}
